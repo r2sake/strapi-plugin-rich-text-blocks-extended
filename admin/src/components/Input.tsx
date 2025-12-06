aaaa188f107a37abeb6ed31cc61ebe84bbf48e15
r2Sake <work_yoimaro@icloud.com>
2025-12-06 18:22:19 +0900

    管理画面不具合修正

--- admin/src/components/Input.tsx
+++ admin/src/components/Input.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { BlocksEditor } from './BlocksInput/BlocksEditor';
-import { Field, Flex } from '@strapi/design-system';
+import { DesignSystemProvider, lightTheme, Field, Flex } from '@strapi/design-system';
 
 
 interface InputProps {
@@ -44,9 +44,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
     try {
       if (props.value) {
         const parsed = typeof props.value === 'string' ? JSON.parse(props.value) : props.value;
-        return Array.isArray(parsed)
-          ? parsed
-          : [{ type: 'paragraph', children: [{ text: '' }] }];
+        return Array.isArray(parsed) ? parsed : [{ type: 'paragraph', children: [{ text: '' }] }];
       }
     } catch (e) {
       console.error('Failed to parse value:', e);
@@ -69,35 +67,37 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   };
 
   return (
-    <Field.Root
-      id={props.name}
-      name={props.name}
-      required={props.required}
-      error={props.error}
-      hint={props?.hint}
-    >
-      <Flex direction="column" alignItems="stretch" gap={1}>
-        <Field.Label>{props.label}</Field.Label>
-        <BlocksEditor
-          ref={ref as any}
-          name={props.name}
-          error={props.error}
-          value={editorValue}
-          ariaLabelId={props.name}
-          disabled={props.disabled}
-          pluginOptions={props.attribute.options}
-          onChange={(eventOrPath, value) => {
-            if (typeof eventOrPath === 'string') {
-              handleChange(eventOrPath, value);
-            } else {
-              handleChange(props.name, eventOrPath.target.value);
-            }
-          }}
-        />
-        <Field.Hint />
-        <Field.Error />
-      </Flex>
-    </Field.Root>
+    <DesignSystemProvider theme={lightTheme}>
+      <Field.Root
+        id={props.name}
+        name={props.name}
+        required={props.required}
+        error={props.error}
+        hint={props?.hint}
+      >
+        <Flex direction="column" alignItems="stretch" gap={1}>
+          <Field.Label>{props.label}</Field.Label>
+          <BlocksEditor
+            ref={ref as any}
+            name={props.name}
+            error={props.error}
+            value={editorValue}
+            ariaLabelId={props.name}
+            disabled={props.disabled}
+            pluginOptions={props.attribute.options}
+            onChange={(eventOrPath, value) => {
+              if (typeof eventOrPath === 'string') {
+                handleChange(eventOrPath, value);
+              } else {
+                handleChange(props.name, eventOrPath.target.value);
+              }
+            }}
+          />
+          <Field.Hint />
+          <Field.Error />
+        </Flex>
+      </Field.Root>
+    </DesignSystemProvider>
   );
 });
 
